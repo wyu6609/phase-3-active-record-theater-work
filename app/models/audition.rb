a95f6a73ff62fcc53407f9c9cbd3d3c2fcc88448
William Yu <wy427@nyu.edu>
2022-03-21 11:54:20 -0400

    Update audition.rb

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,14 +1,29 @@
-class Audition < ActiveRecord::Base
-  belongs_to :role
+class Role < ActiveRecord::Base
+  has_many :auditions
 
-  #role returns an instance of role associated with this audition
-  def role
-    role_id = self.role_id
-    Role.find(role_id)
+  #auditions returns all of the auditions associated with this role
+  def auditions
+    Audition.where(role_id: self.id)
   end
 
-  #call_back will change the the hired attribute to true
-  def call_back
-    Audition.update(hired: true)
+  #actors returns an array of names from the actors associated with this role
+  def actors
+    Audition.pluck(:actor)
   end
+
+  #locations returns an array of locations from the auditions associated with this role
+  def locations
+    Audition.pluck(:location)
+  end
+
+  #lead returns the first instance of the audition that was hired for this role or returns a string 'no actor has been hired for this role'
+  def lead
+    Audition.find_by(hired: true).first || 'no actor has been hired for this role'
+  end
+
+  #understudy returns the second instance of the audition that was hired for this role or returns a string 'no actor has been hired for understudy for this role'
+  def understudy
+    Audition.where(hired: true).second || 'no actor has been hired for this role'
+end
+ end
 end

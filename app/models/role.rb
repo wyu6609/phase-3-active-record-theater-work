a5997d8f90b5dd24017b58d3d1d77dd049757a2c
William Yu <wy427@nyu.edu>
2022-03-23 20:09:52 -0400

    Update role.rb

--- app/models/role.rb
+++ app/models/role.rb
@@ -21,7 +21,7 @@ class Role < ActiveRecord::Base
 
   ##Role#understudy returns the second instance of the audition that was hired for this role or returns a string 'no actor has been hired for understudy for this role'
   def understudy
-    auditions.where(hired: true)[1] || 'no actor has been hired for this role'
+    auditions.where(hired: true).second || 'no actor has been hired for this role'
   end
 
 end
